97f4171bc683e7ceaa010ef13dc5845e9ee914bd
Ricky van den berg <ricky.vandenberg@hotmail.nl>
2017-12-11 12:20:57 +0100

    added basic application

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+import java.sql.*;
+/**
+ * Dit is een voorbeeld Java toepassing waarin je verbinding maakt met een SQLServer database.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
