3cf4be744bc82d10faea2c18aade27cadbb7a262
Ricky van den berg <ricky.vandenberg@hotmail.nl>
2018-01-13 17:37:24 +0100

    Updated Application
    Added Storedprocedure functionality, Added a basic GUI

--- src/DatabaseHandler.java
+++ src/DatabaseHandler.java
@@ -7,61 +7,69 @@ public class DatabaseHandler {
     public static final String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=Statistix;integratedSecurity=true;";
 
 
-    public DatabaseHandler()
-    {
-
-    }
-
-    public void createConnection() {
-
-        // Connection beheert informatie over de connectie met de database.
-        Connection con = null;
-        // Statement zorgt dat we een SQL query kunnen uitvoeren.
-        Statement stmt = null;
-        // ResultSet is de tabel die we van de database terugkrijgen.
-        // We kunnen door de rows heen stappen en iedere kolom lezen.
-        ResultSet rs = null;
-        try {
-            // 'Importeer' de driver die je gedownload hebt.
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            // Maak de verbinding met de database.
-            con = DriverManager.getConnection(connectionUrl);
-            // Stel een SQL query samen.
-            String SQL = "SELECT * FROM Subscriber";
-            stmt = con.createStatement();
-            // Voer de query uit op de database.
-            rs = stmt.executeQuery(SQL);
-            System.out.print(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
-            // Als de resultset waarden bevat dan lopen we hier door deze waarden en printen ze.
-            while (rs.next()) {
-                // Vraag per row de kolommen in die row op.
-                int ISBN = rs.getInt("ISBN");
-                String title = rs.getString("Titel");
-                String author = rs.getString("Auteur");
-                // Print de kolomwaarden.
-                // System.out.println(ISBN + " " + title + " " + author);
-                // Met 'format' kun je de string die je print het juiste formaat geven, als je dat wilt.
-                // %d = decimal, %s = string, %-32s = string, links uitgelijnd, 32 characters breed.
-                System.out.format("| %7d | %-32s | %-24s | \n", ISBN, title, author);
-            }
-            System.out.println(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
-        }
-        // Handle any errors that may have occurred.
-        catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (rs != null) try {
-                rs.close();
-            } catch (Exception e) {
-            }
-            if (stmt != null) try {
-                stmt.close();
-            } catch (Exception e) {
-            }
-            if (con != null) try {
-                con.close();
-            } catch (Exception e) {
-            }
-        }
+    /**
+     *
+     * @param procedure Geef hier het overzicht mee waar je aan werkt.
+     * @param param Word gebruikt indien er een user gegeven nodig is voor de query
+     * @return
+     */
+    public String[] execute(StoredProcedures procedure, String param) {
+//        // Connection beheert informatie over de connectie met de database.
+//        Connection con = null;
+//
+//        // Statement zorgt dat we een SQL query kunnen uitvoeren.
+//        Statement stmt = null;
+//
+//        // ResultSet is de tabel die we van de database terugkrijgen.
+//        // We kunnen door de rows heen stappen en iedere kolom lezen.
+//        ResultSet rs = null;
+//        String SQL;
+//
+//        switch(procedure)
+//        {
+//            case OVERVIEW1:
+//                SQL = "SELECT TOP 10 Episode";
+//                break;
+//            case OVERVIEW2:
+//                break;
+//            case OVERVIEW3:
+//                break;
+//            case OVERVIEW4:
+//                break;
+//            case OVERVIEW5:
+//                break;
+//            case OVERVIEW6:
+//                break;
+//        }
+//
+//        try {
+//            // 'Importeer' de driver die je gedownload hebt.
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            // Maak de verbinding met de database.
+//            con = DriverManager.getConnection(connectionUrl);
+//
+//            // Stel een SQL query samen.
+//            stmt = con.createStatement();
+//            // Voer de query uit op de database.
+//            rs = stmt.executeQuery(SQL);
+//
+//            // Als de resultset waarden bevat dan lopen we hier door deze waarden en printen ze.
+//            while (rs.next()) {
+//
+//            }
+//
+//        }
+//
+//        // Handle any errors that may have occurred.
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        finally {
+//            if (rs != null) try { rs.close(); } catch(Exception e) {}
+//            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
+//            if (con != null) try { con.close(); } catch(Exception e) {}
+//        }
+//    }
+        return null;
     }
 }
---
+++ src/StoredProcedures.java
@@ -0,0 +1,8 @@
+public enum StoredProcedures {
+    OVERVIEW1,
+    OVERVIEW2,
+    OVERVIEW3,
+    OVERVIEW4,
+    OVERVIEW5,
+    OVERVIEW6
+}
